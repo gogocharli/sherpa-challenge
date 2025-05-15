a8ed5e72c1aeaa68fd6b1d05649a9fa9c2dfa097
Charles <thexrayone@icloud.com>
2025-05-15 11:20:26 -0400

    fix: :bug: add the toaster at the root of Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import '../styles/global.css';
+import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
   title: 'Trip Details',
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>{children}</body>
+      <Toaster />
     </html>
   );
 }
